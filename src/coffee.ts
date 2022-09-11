10127269f2ed50b2bfc3209a1c3a1b868c047c17
Harry Fitzgerald <hjfitz@pm.me>
2022-09-11 22:21:28 +0100

    styles and bits

--- src/coffee.ts
+++ src/coffee.ts
@@ -9,11 +9,11 @@ export interface BrewStyle {
 
 export const brewStyles: BrewStyle[] = [
 	{ name: 'Espresso', ratioWater: 2, src: '/espresso.png' },
-	{ name: 'V60', ratioWater: 50, ratioCoffee: 3, src: '/v60.png' },
 	{ name: 'Aeropress', ratioWater: 6, src: '/aeropress.png', },
 	{ name: 'French Press', ratioWater: 12, src: '/french-press.png' },
 	{ name: 'Moka Pot', ratioWater: 10, src: '/moka-pot.png' },
 	{ name: 'Cold Brew', ratioWater: 40, ratioCoffee: 9, src: '/cold-brew.png', },
+	{ name: 'V60', ratioWater: 50, ratioCoffee: 3, src: '/v60.png' },
 ] 
 
 export function getHumanReadableRatio(style: BrewStyle): string {
@@ -21,8 +21,4 @@ export function getHumanReadableRatio(style: BrewStyle): string {
 	return `${ratioCoffee}:${style.ratioWater}`
 }
 
-export function getRatioForCalculation(style: BrewStyle): number {
-	const ratioCoffee = style.ratioCoffee || 1 
-	return ratioCoffee / style.ratioWater
-}
 // <a href="https://www.flaticon.com/free-icons/ice" title="ice icons">Ice icons created by photo3idea_studio - Flaticon</a>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 /* @jsx h */
-import "preact/debug";
+import 'preact/debug'
 
 import {h, render} from 'preact'
-import {useEffect, useRef, useState} from 'preact/hooks'
-import { find, pickBy, propEq } from 'ramda'
+import {useEffect, useState} from 'preact/hooks'
+import { find, propEq } from 'ramda'
 
-import {BrewStyle, brewStyles, getHumanReadableRatio, getRatioForCalculation} from './coffee'
+import {BrewStyle, brewStyles, getHumanReadableRatio} from './coffee'
 
 interface BrewstyleState extends BrewStyle {
 	selected?: boolean
@@ -14,7 +14,7 @@ interface BrewstyleState extends BrewStyle {
 
 type CalculatorProps = BrewStyle
 
-const Calculator = ({ratioWater, ratioCoffee = 1, name}: CalculatorProps) => {
+const Calculator = ({ratioWater, ratioCoffee = 1, name, src}: CalculatorProps) => {
 	const [waterVal, setWaterVal] = useState(ratioWater)
 	const [coffeeVal, setCoffeeVal] = useState(ratioCoffee)
 
@@ -46,10 +46,23 @@ const Calculator = ({ratioWater, ratioCoffee = 1, name}: CalculatorProps) => {
 	}
 
 	return (
-		<div>
-			<h2>{name}: {ratioCoffee}:{ratioWater}</h2>
-			<input type="number" value={coffeeVal}  onKeyUp={onCoffeeChange} />
-			<input type="number" value={waterVal} onKeyUp={onWaterChange} />
+		<div className="flex flex-col h-full">
+			<header className="flex flex-col items-center mt-4">
+				<h2 className="text-4xl text-white ">{name} - {ratioCoffee}:{ratioWater}</h2>
+				<div>
+					<img className="invert h-36 w-auto m-8" src={src} />
+				</div>
+			</header>
+			<section class="flex-1 bg-gray-100 rounded-t-lg flex flex-col items-center justify-evenly">
+				<div class="w-64 pb-8">
+					<label class="block text-zinc-400 text-sm">Coffee</label>
+					<input class="drop-shadow-md rounded px-2 py-1 border-slate-600 w-full" type="number" value={coffeeVal}  onKeyUp={onCoffeeChange} />
+				</div>
+				<div class="w-64">
+					<label class="block text-zinc-400 text-sm">Water</label>
+					<input class="drop-shadow-md rounded px-2 py-1 border-slate-600 w-full" type="number" value={waterVal} onKeyUp={onWaterChange} />
+				</div>
+			</section>
 		</div>
 	)
 }
@@ -57,7 +70,7 @@ const Calculator = ({ratioWater, ratioCoffee = 1, name}: CalculatorProps) => {
 const findSelected = find(propEq('selected', true))
 
 const App = () => {
-	const [styles, setStyles] = useState<BrewstyleState[]>(brewStyles)
+	const [styles, setStyles] = useState<BrewstyleState[]>(brewStyles.map((style, idx) => ({...style, selected: idx === 0})))
 
 	function select(toSelect: BrewstyleState) {
 		return function() {
@@ -66,33 +79,41 @@ const App = () => {
 		}
 	}
 
+	// @ts-expect-error ramda has some of the worst types
 	const selected = findSelected(styles) as BrewstyleState
 
 	return (
-		<main class="container mx-auto max-w-2xl">
-			<header>
-				<h1>Coffee Ratio Calculator</h1>
-			</header>
-			{selected && (
-				<section>
-					<Calculator {...selected} />
+		<main className="h-full flex flex-col justify-between">
+			<div className="bg-indigo-900 flex-1">
+				<section className="container mx-auto max-w-2xl pt-8 h-full flex flex-col">
+					<header className="text-white text-center text-[3rem] font-semibold">
+						<h1>Coffee Ratio Calculator</h1>
+					</header>
+					
+					{selected && (
+						<section class="flex-1">
+							<Calculator {...selected} />
+						</section>
+					)}
 				</section>
-			)}
-			<section class="flex flex-wrap justify-between">
-				{styles.map((style) => {
-					return (
-						<div 
-							class="inline rounded-lg bg-gradient-to-r from-sky-400 to-sky-300 hover:from-sky-500 hover:to-sky-400 ease-linear transition-all py-2 px-4 my-2 cursor-pointer" 
-							onClick={select(style)}
-						>
-							<div class="flex">
-								<p class="inline leading-8 pr-2 text-white">{style.name} <strong>{getHumanReadableRatio(style)}</strong></p>
-								<img class="h-8 inline invert" src={style.src} alt={style.name} />
+			</div>
+			<div className="bg-grey-100 basis-1/3">
+				<section className="container mx-auto max-w-2xl flex flex-wrap justify-between pb-16 items-end min-h-full px-8">
+					{styles.map((style) => {
+						return (
+							<div 
+								className="inline drop-shadow-sm hover:drop-shadow-md rounded-lg bg-gradient-to-r from-sky-400 to-sky-300 hover:from-sky-500 hover:to-sky-400 ease-linear transition-all py-2 px-4 my-2 cursor-pointer" 
+								onClick={select(style)}
+							>
+								<div className="flex">
+									<p className="inline leading-8 pr-2 text-white">{style.name} <strong>{getHumanReadableRatio(style)}</strong></p>
+									<img className="h-8 inline invert" src={style.src} alt={style.name} />
+								</div>
 							</div>
-						</div>
-					)
-				})}
-			</section>
+						)
+					})}
+				</section>
+			</div>
 		</main>
 	)
 }
