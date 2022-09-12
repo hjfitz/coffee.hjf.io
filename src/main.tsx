5a1aaebe15b64b061a5b661e08250649de570ce9
Harry Fitzgerald <hjfitz@pm.me>
2022-09-12 07:59:55 +0100

    important tags go in the <head>

--- src/main.tsx
+++ src/main.tsx
@@ -53,7 +53,7 @@ const Calculator = ({ratioWater, ratioCoffee = 1, name, src}: CalculatorProps) =
 					<img className="invert h-36 w-auto m-8" src={src} />
 				</div>
 			</header>
-			<section class="flex-1 bg-gray-100 rounded-t-lg flex flex-col items-center justify-evenly">
+			<section class="flex-1 bg-gray-100 rounded-t-lg flex flex-col items-center justify-evenly pt-8 pb-4 border border-gray-100">
 				<div class="w-64 pb-8">
 					<label class="block text-zinc-400 text-sm">Coffee</label>
 					<input class="drop-shadow-md rounded px-2 py-1 border-slate-600 w-full" type="number" value={coffeeVal}  onKeyUp={onCoffeeChange} />
@@ -85,7 +85,7 @@ const App = () => {
 	return (
 		<main className="h-full flex flex-col justify-between">
 			<div className="bg-indigo-900 flex-1">
-				<section className="container mx-auto max-w-2xl pt-8 h-full flex flex-col">
+				<section className="container mx-auto max-w-none w-full md:max-w-2xl pt-8 h-full flex flex-col">
 					<header className="text-white text-center text-[3rem] font-semibold">
 						<h1>Coffee Ratio Calculator</h1>
 					</header>
@@ -98,11 +98,11 @@ const App = () => {
 				</section>
 			</div>
 			<div className="bg-grey-100 basis-1/3">
-				<section className="container mx-auto max-w-2xl flex flex-wrap justify-between pb-16 items-end min-h-full px-8">
+				<section className="container mx-auto max-w-2xl flex flex-wrap justify-center pt-4 md:pt-0 bg-gray-100 border border-gray-100 md:justify-between pb-16 items-end min-h-full px-8">
 					{styles.map((style) => {
 						return (
 							<div 
-								className="inline drop-shadow-sm hover:drop-shadow-md rounded-lg bg-gradient-to-r from-sky-400 to-sky-300 hover:from-sky-500 hover:to-sky-400 ease-linear transition-all py-2 px-4 my-2 cursor-pointer" 
+								className="inline drop-shadow-sm hover:drop-shadow-md rounded-lg bg-gradient-to-r from-sky-400 to-sky-300 hover:from-sky-500 hover:to-sky-400 ease-linear transition-all py-2 px-4 m-2 cursor-pointer" 
 								onClick={select(style)}
 							>
 								<div className="flex">
